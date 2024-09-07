d0443d64e077be92fe4f78aaece48975147ff47c
Caroalv <alvaradocarolinad2002@gmail.com>
2024-09-07 08:44:16 -0600

    0709

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
+    <MiComponente /> <!--:value="5"-->
+    <MiComponente2 :value="5" /> 
+
+  </header>
+</template>
+
+<script lang="ts" setup>
+import MiComponente from './components/MiComponente.vue'
+import MiComponente2 from './components/MiComponente2.vue'
+
+</script>
+
+
+<style scoped></style>
